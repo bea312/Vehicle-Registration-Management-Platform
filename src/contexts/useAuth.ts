7d469fc7043216aa8763d95b9424574ade644dfb
bea312 <mukagasirabob@gmail.com>
2026-03-29 11:18:47 -0700

    adding change

--- src/contexts/useAuth.ts
+++ src/contexts/useAuth.ts
@@ -1,12 +1,1 @@
-import { useContext } from 'react'
-import { AuthContext } from './authContextValue.ts'
-
-export const useAuth = () => {
-  const context = useContext(AuthContext)
-
-  if (!context) {
-    throw new Error('useAuth must be used inside AuthProvider')
-  }
-
-  return context
-}
+export { useAuth } from './AuthContext'
